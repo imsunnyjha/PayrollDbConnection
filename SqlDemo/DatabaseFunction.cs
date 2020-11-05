73367ac1a4716a6afe25b568030e605cde461df2
Sunny Jha <sunnyjha11897@gmail.com>
2020-11-05 14:16:31 +0530

    [SUNNY]ADD.using database functions like count,sum,avg,etc on the table emppayroll

---
+++ SqlDemo/DatabaseFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SqlDemo
+{
+    public class DatabaseFunction
+    {
+        public string gender { get; set; }
+        public int count { get; set; }
+        public decimal totalSum { get; set; }
+        public decimal avg { get; set; }
+        public decimal min { get; set; }
+        public decimal max { get; set; }
+    }
+}
--- create database Payroll_Service;.sql
+++
@@ -1,1 +0,0 @@
-create database Payroll_Service;
